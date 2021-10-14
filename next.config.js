d48e7823fec48f277814e5931df46cefde84bccc
borland <gogogo2115@gmail.com>
2021-10-15 00:26:09 +0900

    poweredByHeader 옵션 추가
    poweredByHeader 옵션 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
     compress: true,
+    poweredByHeader: false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
     },
-}
+};
