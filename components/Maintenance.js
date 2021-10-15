6b4e62f86c62f2abe08627c95dbe8655f081c940
borland <gogogo2115@gmail.com>
2021-10-15 19:04:38 +0900

    점검페이지 구성

---
+++ components/Maintenance.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+
+export default function Maintence(){
+    return(
+    <>
+    점검 중입니다.
+    </>);
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
+import Head from "next/head";
+import Maintence from "components/Maintenance";
+
 function NextApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+
+    const isMaintenance = process.env.NEXT_PUBLIC_MAINTENANCE_MODE;
+
+    return (<>
+    <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width" />
+        <title>ssss</title>
+    </Head>
+    {(isMaintenance === "true") 
+    ? (<Maintence />) 
+    : (<Component {...pageProps} />)} 
+    </>);
 }
 export default NextApp;
