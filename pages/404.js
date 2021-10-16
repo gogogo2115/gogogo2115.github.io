e6ccf09fe610d2fa97892718088d264474eaae4f
borland <gogogo2115@gmail.com>
2021-10-17 03:41:32 +0900

    404페이지 redirect 옵션으로 이동 가능하게 설정

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 function Error404(){
-    return(<>존재하지 않는 페이지 입니다.</>);
+
+    const [ isRedirect ] = useState(true);
+    const redirectPath = "/";
+    const router = useRouter();
+
+    useEffect(() => {
+        if(isRedirect === true){
+            router.push(redirectPath);
+        }
+    }, []);
+
+    return((isRedirect === false)
+    ? <>존재하지 않는 페이지 입니다.</>
+    : null);
 }
 export default Error404;
