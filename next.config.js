db83fde98b8b5c7184c4fe9a63bb871ddbf46610
borland <gogogo2115@gmail.com>
2021-10-20 04:20:42 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+const withImages = require('next-images');
+
+const nextConfig = {
     compress: true,
     poweredByHeader: false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
     },
+    images: {
+        disableStaticImages: true,
+    },
 };
+
+module.exports = withPlugins([
+    withBundleAnalyzer, withImages
+
+], nextConfig);
