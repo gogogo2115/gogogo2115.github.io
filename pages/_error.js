07684db3de75cc813be8910337d33570fca841a2
borland <gogogo2115@gmail.com>
2021-10-26 02:44:57 +0900

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,11 +1,11 @@
 function Error({ statusCode }) {
-return (
-    <p>
-    {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
-)
+  return (
+      <p>
+      {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+  );
 }
 
 Error.getInitialProps = ({ res, err }) => {
