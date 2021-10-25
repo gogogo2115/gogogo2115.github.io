c1ca652fbba29ccc75ad16b9a821318cb4a3a858
borland <gogogo2115@gmail.com>
2021-10-26 02:51:54 +0900

    Update appState.js

--- store/appState.js
+++ store/appState.js
@@ -1,20 +1,13 @@
 import { HYDRATE } from 'next-redux-wrapper';
-import { isNavigator } from 'lib/useSSR';
 
 const LOADING = "appState/LOADING";
-const ONLINE = "appState/ONLINE";
 
 const initialState = {
     loading : false,
-    online : isNavigator ? (window.navigator||navigator).onLine : false
 }
 
 export const setIsLoadingStore = (booleanState) => ({
     type : LOADING, isLoading : booleanState
-});
-
-export const setIsOnlineStore = (booleanState) => ({
-    type : ONLINE, isOnline : booleanState
 });
 
 export default function appState(state = initialState, action){
@@ -23,12 +16,7 @@ export default function appState(state = initialState, action){
             return { ...state, ...action.payload.appState }
         case LOADING:
             return { ...state, loading : action.isLoading }
-        case ONLINE:
-            return { ...state, loading : action.isOnline }
         default:
             return state;
     }
-
-
-    return state;
 }
