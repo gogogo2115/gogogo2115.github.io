c65c1591223158e5d4a17735880dd3fa4a6a1bbd
borland <gogogo2115@gmail.com>
2021-10-27 18:04:28 +0900

    next/bundle-analyzer 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
     compress: true,
@@ -11,5 +14,5 @@ const nextConfig = {
 };
 
 module.exports = withPlugins([
-    withImages
+    withBundleAnalyzer, withImages
 ],nextConfig);
