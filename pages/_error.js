31770f8bc54d656b34ac5c4ad31ec01a31cb797b
borland <gogogo2115@gmail.com>
2021-12-06 16:25:05 +0900

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -2,8 +2,8 @@ function Error({ statusCode }) {
   return (
       <p>
       {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
       </p>
   );
 }
