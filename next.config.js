db4b647935345058ea239287804a62c2d7dfd43e
borland <gogogo2115@gmail.com>
2021-12-15 03:01:55 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,12 +8,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
     compress: true,
     //generateEtags: false,
-    //productionBrowserSourceMaps: true,
+    productionBrowserSourceMaps: true,
     poweredByHeader: false,
     esModule: true, //next-images image static url로 출력 옵션
     inlineImageLimit: false, //next-images image static url로 출력 옵션
     images: {disableStaticImages: true}, //next-images 오류방지를 위한 옵션
     sassOptions: { includePaths: [path.join(__dirname, 'styles')] },
+    env: {
+        isDevelopment: process.env.NODE_ENV === "development",
+        isProduction: process.env.NODE_ENV === "production",
+    },
     exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
         return {
           '/': { page: '/', query: { position: 'index' } },
