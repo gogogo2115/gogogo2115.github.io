cd85e54050fe69879a18f12095447e8f86dee069
borland <gogogo2115@gmail.com>
2021-12-15 15:40:07 +0900

    Update isEmpty.js

--- libs/isEmpty.js
+++ libs/isEmpty.js
@@ -1,10 +1,13 @@
-export default function isEmpty(value){
+export default function isEmpty(value, strNullOpt=false){
     if (value === null) return true;
     if (value === undefined || typeof value === 'undefined') return true;
     if (typeof value === 'string' && value === '') return true;
-    // if (typeof value === 'string' && value === 'null') return true;
-    // if (typeof value === 'string' && value === 'undefined') return true;
     if (Array.isArray(value) && value.length < 1) return true;
     if (typeof value === 'object' && Object.keys(value).length < 1) return true;
+    if(strNullOpt === true){
+        if (typeof value === 'string' && value.trim() === 'null') return true;
+        if (typeof value === 'string' && value.trim() === 'undefined') return true;
+
+    }
     return false;
 }
