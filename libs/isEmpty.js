24c1fde134acbbeca2caedf013615d969b9b275d
borland <gogogo2115@gmail.com>
2021-12-18 02:50:28 +0900

    object에 대한 정규식 추가
    object가 string일때 처리 정규식 추가

--- libs/isEmpty.js
+++ libs/isEmpty.js
@@ -1,13 +1,15 @@
-export default function isEmpty(value, strNullOpt=false){
+export default function isEmpty(value, strOpt=false){
+
     if (value === null) return true;
     if (value === undefined || typeof value === 'undefined') return true;
     if (typeof value === 'string' && value === '') return true;
     if (Array.isArray(value) && value.length < 1) return true;
     if (typeof value === 'object' && Object.keys(value).length < 1) return true;
-    if(strNullOpt === true){
-        if (typeof value === 'string' && value.trim() === 'null') return true;
-        if (typeof value === 'string' && value.trim() === 'undefined') return true;
-
+    if(strOpt === true){
+        if ( typeof value === 'string' && /^null$/i.test(value.trim()) ) return true;
+        if ( typeof value === 'string' && /^undefined$/i.test(value.trim()) ) return true;
+        if ( typeof value === 'string' && /^\{(\s*)\}$/g.test(value.trim()) ) return true;
+        if ( typeof value === 'string' && /^\[(\s*)\]$/g.test(value.trim()) ) return true;
     }
     return false;
 }
