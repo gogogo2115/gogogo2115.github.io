56d79e5f65a6d5b8c78b1c76d9e3b943486d332e
borland <gogogo2115@gmail.com>
2022-06-07 21:24:21 +0900

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,1 +1,16 @@
-_
+function Error({ statusCode }) {
+  return (
+    <div className="Error">
+      {statusCode
+        ? `서버에서 ${statusCode} 오류가 발생했습니다.`
+        : "클라이언트에서 오류가 발생했습니다."}
+    </div>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
