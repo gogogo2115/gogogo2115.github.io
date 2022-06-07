61bf929b4239fbdcb5b0094f993d3dde4732f08c
borland <gogogo2115@gmail.com>
2022-06-07 21:36:34 +0900

    최신 업데이트 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,100 +1,103 @@
+import { useEffect, useCallback, useState } from "react";
+import throttle from "lodash/throttle";
 
-import { useEffect, useCallback, useState} from 'react';
+import { useRouter } from "next/router";
 
-import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from "react-redux";
 
-import { useDispatch, useSelector } from 'react-redux'; 
+import wrapper from "store";
+import toBoolean from "libs/toBoolean";
 
-import wrapper from 'store';
-import toBoolean from 'libs/toBoolean';
-
-import Maintenance from 'components/Maintenance';
-import Loading from 'components/Loading';
-import AppHead from 'components/AppHead';
-import { isNavigator as getIsNavigator } from 'libs/useSSR';
-import { storeSetIsLoading, storeSetIsOnline } from 'store/appState';
+import Maintenance from "components/Maintenance";
+import Loading from "components/Loading";
+import AppHead from "components/AppHead";
+import { isNavigator as getIsNavigator } from "libs/useSSR";
+import { storeSetIsLoading, storeSetIsOnline } from "store/appState";
 import DevModeView from "components/DevModeView";
 
-import 'public/reset.css';
+import "public/reset.css";
 import Modal from "components/Modal";
 
 function NextAPP({ Component, pageProps }) {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const appStateLoading = useSelector((state) => state.appState.loading);
+  const appWindowDeviceType = useSelector(
+    (state) => state.appWindow.deviceType
+  );
 
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const appStateLoading = useSelector(state => state.appState.loading);
-    const appWindowDeviceType = useSelector(state => state.appWindow.deviceType);
-    
-    const isMaintenanceMode = toBoolean(process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
+  const isMaintenanceMode = toBoolean(process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
 
-    //<!-- window addEventListener
-    const handleResizeEvent = useCallback((e) => {
-        e.preventDefault();
-        return false;
-    },[]);
+  //<!-- window addEventListener
+  const handleResizeEvent = useCallback((e) => {
+    e.preventDefault();
+    return false;
+  }, []);
 
-    const handleScrollEvent = useCallback((e) => {
-        e.preventDefault();
-        return false;
-    },[]);
-    // window addEventListener -->
+  const handleScrollEvent = useCallback((e) => {
+    e.preventDefault();
+    return false;
+  }, []);
+  // window addEventListener -->
 
-    //<!-- router events
-    const handleRouteStart = useCallback(() => {
-        //console.log('handleRouteStart');
-        dispatch(storeSetIsLoading(true));
-        return false;
-    },[dispatch]);
+  //<!-- router events
+  const handleRouteStart = useCallback(() => {
+    //console.log('handleRouteStart');
+    dispatch(storeSetIsLoading(true));
+    return false;
+  }, [dispatch]);
 
-    const handleRouteComplete = useCallback(() => {
-        //console.log('handleRouteComplete');
-        const isOnline = getIsNavigator() ? navigator.onLine : true;
-        dispatch(storeSetIsLoading(false));
-        dispatch(storeSetIsOnline(isOnline));
-        return false;
-    },[dispatch]);
-    // router events -->
+  const handleRouteComplete = useCallback(() => {
+    //console.log('handleRouteComplete');
+    const isOnline = getIsNavigator() ? navigator.onLine : true;
+    dispatch(storeSetIsLoading(false));
+    dispatch(storeSetIsOnline(isOnline));
+    return false;
+  }, [dispatch]);
+  // router events -->
 
-    useEffect(() => {
-        window.addEventListener('resize',throttle(handleResizeEvent,450));
-        window.addEventListener('scroll',throttle(handleScrollEvent,450));
-        return () => {
-            window.removeEventListener('resize',throttle(handleResizeEvent,450));
-            window.removeEventListener('scroll',throttle(handleScrollEvent,450));
-        }    
-    }, []);
+  useEffect(() => {
+    window.addEventListener("resize", throttle(handleResizeEvent, 450));
+    window.addEventListener("scroll", throttle(handleScrollEvent, 450));
+    return () => {
+      window.removeEventListener("resize", throttle(handleResizeEvent, 450));
+      window.removeEventListener("scroll", throttle(handleScrollEvent, 450));
+    };
+  }, []);
 
-    useEffect(() => {
-        router.events.on('routeChangeStart', handleRouteStart);
-        router.events.on('routeChangeComplete', handleRouteComplete);
-        router.events.on('routeChangeError', handleRouteComplete);
-    
-        return () => {
-          router.events.off('routeChangeStart', handleRouteStart);
-          router.events.off('routeChangeComplete', handleRouteComplete);
-          router.events.off('routeChangeError', handleRouteComplete);
-        }
-    }, [router.events]);
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteComplete);
+    router.events.on("routeChangeError", handleRouteComplete);
 
-    return(
+    return () => {
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteComplete);
+      router.events.off("routeChangeError", handleRouteComplete);
+    };
+  }, [router.events]);
+
+  return (
     <>
-    <DevModeView />
+      <DevModeView />
 
-    <AppHead />
+      <AppHead />
 
-    <noscript id="noScript">
-        모든 기능을 활용하기 위해서는 자바스크립트(JavaScript) 활성화가 필요합니다.
-    </noscript>
+      <noscript id="noScript">
+        모든 기능을 활용하기 위해서는 자바스크립트(JavaScript) 활성화가
+        필요합니다.
+      </noscript>
 
-    {(isMaintenanceMode === false)
-        ? (<Maintenance />)
-        : (<Component {...pageProps} />)}
-    
-    <Modal />
+      {isMaintenanceMode === false ? (
+        <Maintenance />
+      ) : (
+        <Component {...pageProps} />
+      )}
 
-    {(appStateLoading === true) 
-        ? (<Loading />) 
-        : (null)}
-    </>);
+      <Modal />
+
+      {appStateLoading === true ? <Loading /> : null}
+    </>
+  );
 }
 export default wrapper.withRedux(NextAPP);
