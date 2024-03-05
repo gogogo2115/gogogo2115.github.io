8c667812cac4924e4904b36420ef02a53a379f8e
borland <46901223+gogogo2115@users.noreply.github.com>
2024-03-05 22:35:18 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === "development";
+const IS_PROD = NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: false,
+  compiler: {
+    removeConsole: IS_PROD,
+  },
+};
 
 export default nextConfig;
