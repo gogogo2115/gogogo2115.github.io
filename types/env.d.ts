764e9f049b459f965d0c0f1be5274885ec7bde33
borland <46901223+gogogo2115@users.noreply.github.com>
2024-03-06 01:20:48 +0900

    환경변수 조정

--- types/env.d.ts
+++ types/env.d.ts
@@ -5,5 +5,11 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_PACKAGE_NAME: string;
     readonly NEXT_PUBLIC_PACKAGE_VERSION: string;
     readonly NEXT_PUBLIC_IS_MAINTENANCE: string;
+
+    readonly PACKAGE_GENERATOR: string;
+    readonly PACKAGE_HASH: string;
+    readonly BUILD_AT: string;
+    readonly BUILD_TS: string;
+    readonly BUILD_HASH: string;
   }
 }
