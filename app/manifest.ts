7cdf659217e335e4eee6a25ed8284e9a6e98b260
borland <46901223+gogogo2115@users.noreply.github.com>
2024-03-06 13:00:05 +0900

    업데이트

---
+++ app/manifest.ts
@@ -0,0 +1,18 @@
+import { type MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Next.js App!",
+    short_name: "Next.js App!",
+    description: "Next.js App!",
+    start_url: "/",
+    display: "standalone",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
