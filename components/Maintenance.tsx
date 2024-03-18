b1c0366f036a9909fc5cf57d8354ea8b72442414
borland <46901223+gogogo2115@users.noreply.github.com>
2024-03-18 19:37:17 +0900

    Update Maintenance.tsx

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -1,13 +1,21 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { isNavigatorShareSupported } from "@/hooks/NavigatorShare";
 
 const Maintenance = () => {
+  const [test, setTest] = useState(false);
+
   useEffect(() => {
     console.log("isNavigatorShareSupported", isNavigatorShareSupported());
+    setTest(isNavigatorShareSupported());
   }, []);
 
-  return <div id="Maintenance">준비중입니다.</div>;
+  return (
+    <div>
+      <div>{test ? "테스트!" : "테스트!!"}</div>
+      <div id="Maintenance">준비중입니다.</div>
+    </div>
+  );
 };
 
 export default Maintenance;
