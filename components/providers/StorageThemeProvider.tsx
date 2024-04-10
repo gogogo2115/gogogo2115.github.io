34db5d958d5f14c88ff10ba43b29d168759d0a7d
borland <46901223+gogogo2115@users.noreply.github.com>
2024-04-11 02:44:06 +0900

    Update StorageThemeProvider.tsx

--- components/providers/StorageThemeProvider.tsx
+++ components/providers/StorageThemeProvider.tsx
@@ -1,21 +1,78 @@
 "use client";
 import useIsomorphicLayoutEffect from "@/hooks/useIsomorphicEffect";
-import { useAppStoreSelector } from "@/store";
-import { selectAppStateTheme } from "@/store/modules/appState";
-import { Fragment, type ReactNode } from "react";
+import { useAppStoreDispatch, useAppStoreSelector } from "@/store";
+import { appStateActions, selectAppStateTheme } from "@/store/modules/appState";
+import { Fragment, useCallback, type ReactNode } from "react";
+import { shallowEqual } from "react-redux";
 
 type ProviderProps = {
   children: ReactNode;
 };
 
+const { setTheme } = appStateActions;
+
+export type ThemeColor = "dark" | "light" | "system" | "gray";
+type ThemeColorObj = Record<ThemeColor, { value: ThemeColor; num: number }>;
+
+const THEME_COLOR_ARR: ThemeColor[] = ["dark", "light", "system", "gray"];
+export const DEFAULT_THEME_COLOR: (typeof THEME_COLOR_ARR)[number] = "system";
+
+const THEME_COLOR_OBJ: ThemeColorObj = THEME_COLOR_ARR.reduce((acc, curr, index) => {
+  acc[curr] = { value: curr, num: index };
+  return acc;
+}, {} as ThemeColorObj);
+
+const STORAGE_NAME = "theme";
+const DATASET_NAME = "theme";
+
+const isValidThemeColor = (theme?: string) => {
+  return THEME_COLOR_ARR.includes(theme as ThemeColor);
+};
+
+const COLOR_SCHEME_QUERY = {
+  dark: "(prefers-color-scheme: dark)",
+  light: "(prefers-color-scheme: light)",
+  /**
+   * @deprecated dark 또는 light 값을 사용해주세요
+   */
+  noPreference: "(prefers-color-scheme: no-preference)",
+};
+
+// const getPrefersColorScheme = () => {
+//   const isMatches = window.matchMedia("(prefers-color-scheme: dark)").matches;
+//   const { dark, light } = COLOR_THEME_OBJ;
+//   return isMatches ? dark.value : light.value;
+// };
+
+// const resolveSystemThemeColor = (themeColor: ThemeColor): ThemeColor => {
+//   return themeColor === "system" ? getPrefersColorScheme() : themeColor;
+// };
+
 const StorageThemeProvider = ({ children }: ProviderProps) => {
-  const theme = useAppStoreSelector(selectAppStateTheme);
+  const currTheme = useAppStoreSelector(selectAppStateTheme, shallowEqual) as ThemeColor;
 
   useIsomorphicLayoutEffect(() => {
-    console.log(theme);
-  }, []);
+    if (currTheme !== "system") return;
+    const darkColorScheme = window.matchMedia(COLOR_SCHEME_QUERY.dark);
+  }, [currTheme]);
 
   return <Fragment key={"StorageThemeProvider"}>{children}</Fragment>;
+};
+
+export const useStorageTheme = () => {
+  const currTheme = useAppStoreSelector(selectAppStateTheme, shallowEqual);
+  const dispatch = useAppStoreDispatch();
+
+  const applyTheme = useCallback(
+    (themeColor: ThemeColor) => {
+      if (currTheme === themeColor) return;
+      dispatch(setTheme(themeColor));
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currTheme]
+  );
+
+  return { currTheme, applyTheme };
 };
 
 export default StorageThemeProvider;
