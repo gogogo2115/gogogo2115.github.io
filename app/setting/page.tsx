3259bce87eaddd3229de6ff24edb32365f54efb9
borland <46901223+gogogo2115@users.noreply.github.com>
2024-04-14 23:46:03 +0900

    테마 적용 관련 일부 추가

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,4 +1,7 @@
-import { PACKAGE_GENERATOR, BUILD_HASH, BUILD_AT } from "@/utils/index";
+"use client";
+
+import { BUILD_HASH, BUILD_AT } from "@/utils/index";
+import { MouseEvent } from "react";
 
 const SettingPage = () => {
   const utcDate = new Date(BUILD_AT);
@@ -13,11 +16,33 @@ const SettingPage = () => {
     weekday: "long",
   });
 
+  const onClickThemeBtn = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const target = e.currentTarget;
+    const value = target.value;
+    const dataTheme = target.dataset["theme"];
+    alert(dataTheme);
+  };
+
   return (
-    <ul>
-      <li>build hash : {BUILD_HASH}</li>
-      <li>build date : {BUILD_AT_KR}</li>
-    </ul>
+    <div>
+      <div>
+        <button type="button" data-theme="dark" value={"dark"} onClick={onClickThemeBtn} title="">
+          다크
+        </button>
+        <button type="button" data-theme="light" value={"light"} onClick={onClickThemeBtn} title="">
+          라이트
+        </button>
+        <button type="button" data-theme="system" value={"system"} onClick={onClickThemeBtn} title="">
+          시스템
+        </button>
+
+        <ul>
+          <li>build hash : {BUILD_HASH}</li>
+          <li>build date : {BUILD_AT_KR}</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
