c1c86a684a537f8c8c6a83f5c0808e9f47f79c10
borland <46901223+gogogo2115@users.noreply.github.com>
2024-04-16 03:55:20 +0900

    테스트 페이지 업로드

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import Giscus, { type GiscusProps, type Repo } from "@giscus/react";
+
+const initial: GiscusProps = {
+  repo: process.env.NEXT_PUBLIC_GISCUS_REPO as Repo,
+  repoId: process.env.NEXT_PUBLIC_GISCUS_REPO_ID,
+  category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+  categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+  mapping: "pathname",
+  lang: "ko",
+};
+
 const GuestbookPage = () => {
-  return <></>;
+  return (
+    <div style={{ width: "80%", margin: "8px auto" }}>
+      <Giscus {...initial} />
+    </div>
+  );
 };
 
 export default GuestbookPage;
