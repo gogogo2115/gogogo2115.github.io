5ce25df4e9199cccb8023c548d8dfd3190b0de09
borland <46901223+gogogo2115@users.noreply.github.com>
2024-04-15 22:36:19 +0900

    Update Maintenance.tsx

--- components/Maintenance.tsx
+++ components/Maintenance.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { MouseEvent, Suspense, useCallback, useEffect, useState } from "react";
+import { Suspense, useEffect } from "react";
 import styled from "styled-components";
-import { useStorageTheme } from "./providers/StorageThemeProvider";
+// import { useStorageTheme } from "./providers/StorageThemeProvider";
 
 const StyledMaintenance = styled.div`
   position: relative;
@@ -25,27 +25,27 @@ const AsyncDataFetcher = () => {
 };
 
 const Maintenance = () => {
-  const { curr, applyTheme } = useStorageTheme();
+  // const { curr, applyTheme } = useStorageTheme();
 
   useEffect(() => {
     console.log("aaaa");
   }, []);
 
-  const onClickThemeBtn = useCallback(
-    (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      const target = e.currentTarget;
-      const theme = target.dataset["theme"] || target.value;
-      applyTheme(theme as "dark" | "light" | "system");
-    },
-    [applyTheme]
-  );
+  // const onClickThemeBtn = useCallback(
+  //   (e: MouseEvent<HTMLButtonElement>) => {
+  //     e.preventDefault();
+  //     const target = e.currentTarget;
+  //     const theme = target.dataset["theme"] || target.value;
+  //     applyTheme(theme as "dark" | "light" | "system");
+  //   },
+  //   [applyTheme]
+  // );
 
   return (
     <StyledMaintenance id="Maintenance">
       <div>준비중입니다. </div>
 
-      <div>
+      {/* <div>
         <button type="button" data-theme="dark" value={"dark"} onClick={onClickThemeBtn} title="다크 테마" aria-label="다크 테마">
           다크
         </button>
@@ -55,7 +55,7 @@ const Maintenance = () => {
         <button type="button" data-theme="system" value={"system"} onClick={onClickThemeBtn} title="시스템 테마" aria-label="다크 테마">
           시스템
         </button>
-      </div>
+      </div> */}
       <div>
         Hello{" "}
         <Suspense fallback="...">
