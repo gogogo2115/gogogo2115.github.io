a8a55209b2187edefd81a43e3a41e04ec944551b
borland <46901223+gogogo2115@users.noreply.github.com>
2024-04-30 12:23:09 +0900

    수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,19 +40,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <AppStoreProvider>
       <StyledComponentsProvider>
-        <>
+        <StorageThemeProvider>
           <html lang="ko" dir="ltr">
             <body id="__next" className={nanum.className}>
               <ReactQueryProvider>
-                <StorageThemeProvider>{IS_PROD_MAINTENANCE ? <Maintenance /> : children}</StorageThemeProvider>
+                <>{IS_PROD_MAINTENANCE ? <Maintenance /> : children}</>
               </ReactQueryProvider>
-              <script type="text/javascript" id="storageThemeScript" defer async src="/storageTheme.js" />
+              {<script type="text/javascript" id="storageThemeScript" defer async src="/storageTheme.js" />}
               <script type="text/javascript" id="featureNotBug" defer async src="/featureNotBug.js" />
               <GoogleAnalytics />
               <WebVitals />
             </body>
           </html>
-        </>
+        </StorageThemeProvider>
       </StyledComponentsProvider>
     </AppStoreProvider>
   );
