ed66aa03b1306e2099c8fec65c6b77ce9e2c0cbb
borland <46901223+gogogo2115@users.noreply.github.com>
2024-05-14 00:05:30 +0900

    Update page.tsx

--- app/web-safe-colors/page.tsx
+++ app/web-safe-colors/page.tsx
@@ -1,27 +1,50 @@
-import Link from "next/link";
 import { type RGB_OBJ, generateWebSafeColors } from "../func/color/colorUtils";
 
-const webSafeHexColors = generateWebSafeColors({ returnType: "OBJECT" }) as {
+const webSafeObjColors = generateWebSafeColors({ returnType: "OBJECT" }) as {
   hex: string;
-  rgb: RGB_OBJ;
+  rgb: Omit<RGB_OBJ, "a">;
 }[];
 
+const dataLength = webSafeObjColors.length ?? 0;
+
 export default function WebSafeColorsPage() {
-  return (
-    <div>
-      {webSafeHexColors.map((v, index) => {
-        const { hex, rgb } = v;
-        const hexColor = `#${hex}`;
-        return (
-          <div key={index} data-index={index} style={{ display: "inline-flex", flexDirection: "column", gap: "2px", margin: "2px" }}>
-            <div style={{ backgroundColor: hexColor, width: "64px", height: "64px", borderRadius: "8px" }} />
-            <div style={{ fontSize: "1.3rem", textAlign: "center" }}>{hexColor}</div>
-            <Link href={`/web-safe-colors/${hex}`}>
-              {rgb?.r}/{rgb?.g}/{rgb?.b}
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>{dataLength}</div>;
 }
+
+// {webSafeObjColors.map((v, index) => {
+//   const { hex, rgb } = v;
+//   const hexColor = `#${hex}`;
+//   return (
+//     <div key={index} id={`${hexColor}`} data-index={index} style={{ display: "inline-flex", flexDirection: "column", gap: "2px", margin: "2px" }}>
+//       <div style={{ backgroundColor: hexColor, width: "64px", height: "64px", borderRadius: "8px" }} />
+//       <div style={{ fontSize: "1.3rem", textAlign: "center" }}>{hexColor}</div>
+//       <Link href={`/web-safe-colors/${hex}`}>
+//         {rgb?.r}/{rgb?.g}/{rgb?.b}
+//       </Link>
+//     </div>
+//   );
+// })}
+// const 绿色_蓝色 = () => {
+//   const rgb = webSafeObjColors.filter((v) => v.rgb.r === 0);
+//   return rgb;
+// };
+
+// const 红色_蓝色 = () => {
+//   const rgb = webSafeObjColors.filter((v) => v.rgb.g === 0);
+//   return rgb;
+// };
+
+// const 红色_绿色 = () => {
+//   const rgb = webSafeObjColors.filter((v) => v.rgb.b === 0);
+//   return rgb;
+// };
+
+// const 柔和色 = () => {
+//   const rgb = webSafeObjColors.filter((v) => v.rgb.r >= 102 && v.rgb.g >= 102 && v.rgb.b >= 102);
+//   return rgb;
+// };
+
+// const 灰色 = () => {
+//   const rgb = webSafeObjColors.filter((v) => v.rgb.r == v.rgb.g && v.rgb.g == v.rgb.b);
+//   return rgb;
+// };
