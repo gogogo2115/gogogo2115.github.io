7282f6b1e84719c0fac26d56046d728d3388cead
borland <46901223+gogogo2115@users.noreply.github.com>
2024-06-01 00:57:49 +0900

    Update page.tsx

--- app/colors/web-safe/[color]/page.tsx
+++ app/colors/web-safe/[color]/page.tsx
@@ -5,15 +5,10 @@ type PageProps = {
   searchParams: { [string: string]: string };
 };
 
-const isColorHexRegex = (color?: string) => {
-  const regex = /^#?([0-9A-F]{3}([0-9A-F]{1})|[0-9A-F]{6}([0-9A-F]{2})?)$/i;
-  return regex.test(color as string);
-};
-
 export default function ColorsWebSafeColorPage(props: PageProps) {
   const { params } = props;
   const pramsColor = params.color;
 
-  if (!isColorHexRegex(pramsColor)) return notFound();
+  if (false) return notFound();
   return <>{pramsColor}</>;
 }
