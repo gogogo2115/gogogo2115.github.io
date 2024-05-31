37ff5c7d0019ef6b8e39c754aa0bb566a09342d1
borland <46901223+gogogo2115@users.noreply.github.com>
2024-05-31 13:29:08 +0900

    데이터 타입 정의

--- app/web-safe-colors/page.tsx
+++ app/web-safe-colors/page.tsx
@@ -3,8 +3,28 @@
 import { webSafeColorsData } from "@/app/web-safe-colors/webSafeColorsData";
 
 const webSafeData = webSafeColorsData();
-console.log(webSafeData);
 
 export default function WebSafeColors() {
-  return <></>;
+  return (
+    <>
+      {webSafeData.map((v, i) => {
+        const dataKey = `${v.key.r}${v.key.g}${v.key.b}`;
+        const dataHex = `${v.hex.r}${v.hex.g}${v.hex.b}`;
+        const backgroundColor = `rgb(${v.rgb.r}, ${v.rgb.g}, ${v.rgb.b})`;
+        return (
+          <div
+            key={i}
+            data-idx={v.idx}
+            data-key={dataKey}
+            data-hex={dataHex}
+            style={{
+              backgroundColor,
+            }}
+          >
+            {i}
+          </div>
+        );
+      })}
+    </>
+  );
 }
