36bacca3299da8cbf16a7b39e9f4c48bdb59d55a
borland <46901223+gogogo2115@users.noreply.github.com>
2024-06-02 23:01:42 +0900

    color hex 타입 검증 추가

--- app/colors/web-safe/[color]/not-found.tsx
+++ app/colors/web-safe/[color]/not-found.tsx
@@ -3,5 +3,6 @@ import { useParams } from "next/navigation";
 
 export default function WebSafeColorNotFound() {
   const { color = "" } = useParams();
+  const isColorHexRegex = /^([0-9A-F]{3}|[0-9A-F]{6})$/i;
   return <>{color}</>;
 }
