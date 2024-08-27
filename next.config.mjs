aa20fa0b22fde5bb839029723666955020d8d768
borland <46901223+gogogo2115@users.noreply.github.com>
2024-08-28 04:08:52 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,12 +13,12 @@ const nextConfig = {
   reactStrictMode: false,
   output: "export",
   sassOptions: { includePaths: [path.join(__dirname, "styles")] },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }, // ignoreDuringBuilds : true eslint가 오류시 강제 빌드 react-query 관련 설정
+  typescript: { ignoreBuildErrors: true }, // ignoreDuringBuilds : true  eslint가 오류시 강제 빌드 react-query 관련 설정
   webpack: (config) => {
     /*
-    config.cache = false; 관련
-    [webpack.cache.PackFileCacheStrategy] Caching failed for pack: Error: Unable to snapshot resolve dependencies
+      config.cache = false; 관련
+      [webpack.cache.PackFileCacheStrategy] Caching failed for pack: Error: Unable to snapshot resolve dependencies
     */
     config.cache = false;
     if (config.name === "server") config.optimization.concatenateModules = false; // react-query 관련 설정
