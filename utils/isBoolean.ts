9ecebefc92b65dbfaddb055b81d68dd4a3f8813d
borland <46901223+gogogo2115@users.noreply.github.com>
2024-08-28 00:30:43 +0900

    소스 개선

--- utils/isBoolean.ts
+++ utils/isBoolean.ts
@@ -1,15 +1,17 @@
-type ToBooleanOptions = { isErrNull: boolean; isStrict: boolean };
+type ToBooleanOptions = { isErrNull?: boolean; isStrict?: boolean };
 
 export const isBoolean = (value: string = "", isStrict: boolean = true) => {
   value = String(value).toLowerCase().trim();
   const strictBooleans = ["true", "false", "1", "0"];
-  const looseBooleans = [...strictBooleans, "yes", "no", "y", "n", "on", "off"];
-  return isStrict ? strictBooleans.includes(value) : looseBooleans.includes(value);
+  const notStrictBooleans = [...strictBooleans, "yes", "no", "y", "n", "on", "off"];
+  return isStrict ? strictBooleans.includes(value) : notStrictBooleans.includes(value);
 };
 
-export const toBoolean = (value: string | boolean | number, options: ToBooleanOptions = { isErrNull: false, isStrict: true }) => {
+const toBooleanOptions = { isErrNull: false, isStrict: true };
+
+export const toBoolean = (value: string | boolean | number, options: ToBooleanOptions = toBooleanOptions) => {
   value = String(value).toLowerCase();
-  const { isErrNull, isStrict } = options;
+  const { isErrNull = toBooleanOptions.isErrNull, isStrict = toBooleanOptions.isStrict } = options;
   if (isErrNull && !isBoolean(value, isStrict)) return null;
-  return isStrict ? /^(true|1)$/i.test(value.trim()) : !/^(false|0|no|n|off)$/i.test(value.trim()) && value !== "";
+  return isStrict ? /^(true|1)$/i.test(value.trim()) : !/^(false|0|no|n|off|undefined|null|\[object Object\])$/i.test(value.trim()) && value !== "";
 };
