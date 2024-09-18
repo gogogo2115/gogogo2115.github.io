026c91a97414785d62e1a46816376455b63c8d2f
borland <46901223+gogogo2115@users.noreply.github.com>
2024-09-19 01:42:54 +0900

    작업 파일 생성

---
+++ components/google/GoogleTagManager.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
+
+type GoogleTagManagerProps = { gtmId: string | undefined | null };
+
+export function GoogleTagManager({ gtmId }: GoogleTagManagerProps) {
+  return <></>;
+}
