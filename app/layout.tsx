53c56756058c30fea9161e6b69ae2da26fc18158
borland <46901223+gogogo2115@users.noreply.github.com>
2024-09-21 01:59:20 +0900

    리덕스 관련 추가와 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import "@/styles/globals.scss";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/styles/globals.scss";
-import { toBoolean } from "@/utils/isBoolean";
+import { ReactQueryStreamedProvider } from "@/components/react-query/ReactQueryProvider";
+import GoogleAnalytics from "@/components/google/GoogleAnalytics";
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 const inter = Inter({ subsets: ["latin"] });
-const NEXT_VERSION = process.env.NEXT_VERSION || "";
+const NEXT_VERSION = process.env.NEXT_VERSION;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,9 +19,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="ko">
+    <html lang="ko" dir="ltr">
       <body id="__next" className={inter.className}>
-        {children}
+        <ReactQueryStreamedProvider>{children}</ReactQueryStreamedProvider>
+        <GoogleAnalytics />
       </body>
     </html>
   );
