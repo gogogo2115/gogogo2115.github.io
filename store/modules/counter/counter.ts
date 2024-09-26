5780af0569b1d55aa97cb139ea9a02e03964925c
borland <46901223+gogogo2115@users.noreply.github.com>
2024-09-26 12:34:39 +0900

    웹 폰트 대비 색상 구하기

---
+++ store/modules/counter/counter.ts
@@ -0,0 +1,24 @@
+import { createAppSlice } from "@/store";
+
+export interface CounterSliceState {
+  value: number;
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: CounterSliceState = {
+  value: 0,
+  status: "idle",
+};
+
+export const counterSlice = createAppSlice({
+  name: "counter",
+  initialState,
+  reducers: (create) => ({
+    increment: create.reducer((state) => {
+      state.value += 1;
+    }),
+    decrement: create.reducer((state) => {
+      state.value -= 1;
+    }),
+  }),
+});
