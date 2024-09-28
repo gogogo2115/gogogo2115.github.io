569aca4c836c48a05183aa3fb71468fbcabdb2b2
borland <46901223+gogogo2115@users.noreply.github.com>
2024-09-29 02:08:20 +0900

    웹 컬러 페이지 추가

--- app/colors/web/page.client.tsx
+++ app/colors/web/page.client.tsx
@@ -1,3 +1,52 @@
-export default function ColorsWebPageClient() {
-  return <>colorsWebPage</>;
+"use client";
+
+import { copyToClipboard, isCopyClipboardSupported } from "@/utils/copyToClipboard";
+import { type Data } from "./data";
+
+type ColorsWebPageClientProps = { data: Data[] };
+
+export default function ColorsWebPageClient({ data }: ColorsWebPageClientProps) {
+  const isCopySupported = isCopyClipboardSupported();
+
+  return (
+    <div>
+      {data.map((v, i) => {
+        const {
+          name,
+          hex,
+          rgb: { r, g, b },
+        } = v;
+        const rgbText = `rgb(${r},${g},${b})`;
+
+        return (
+          <div key={i} className="w-[240px] h-[200px] rounded-xl p-2 block text-outline border-white border-solid border-[1px]" style={{ backgroundColor: name }}>
+            <div className="flex flex-row justify-between">
+              <div>{name}</div>
+              {isCopySupported && (
+                <button type="button" className="text-black" title={`${name} 복사`} onClick={() => copyToClipboard(name)}>
+                  복사
+                </button>
+              )}
+            </div>
+            <div className="flex flex-row justify-between">
+              <div>{hex}</div>
+              {isCopySupported && (
+                <button type="button" className="text-black" title={`${hex} 복사`} onClick={() => copyToClipboard(hex)}>
+                  복사
+                </button>
+              )}
+            </div>
+            <div className="flex flex-row justify-between">
+              <div>{rgbText}</div>
+              {isCopySupported && (
+                <button type="button" className="text-black" title={`${rgbText} 복사`} onClick={() => copyToClipboard(rgbText)}>
+                  복사
+                </button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- app/colors/web/page.tsx
+++ app/colors/web/page.tsx
@@ -1,5 +1,18 @@
 import ColorsWebPageClient from "@/app/colors/web/page.client";
+import { data } from "./data";
+
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "웹 컬러 140개 색상 이름, Hex 및 RGB 코드",
+  description: "웹 컬러 이름, Hex 및 RGB 코드를 제공하는 필수 색상 참조 페이지. 140개의 웹 안전 색상을 한눈에 확인하세요.",
+  keywords: ["HTML 색상 코드", "웹 컬러 목록", "웹 색상 이름", "140개 웹 색상"],
+};
 
 export default function ColorsWebPage() {
-  return <ColorsWebPageClient />;
+  const defaultData = data.sort((a, b) => a.name.localeCompare(b.name));
+  const length = defaultData.length ?? 0;
+  if (length >= 1) return <ColorsWebPageClient data={defaultData} />;
+  return notFound();
 }
