751b5a27542365cc42137b88af3d069074a49983
borland <46901223+gogogo2115@users.noreply.github.com>
2024-09-30 02:35:46 +0900

    디자인은 어렵다

--- app/colors/web-safe/page.client.tsx
+++ app/colors/web-safe/page.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { notFound } from "next/navigation";
-import { useState, MouseEvent, useMemo } from "react";
+import { useState, useMemo } from "react";
 
 import { type WebSafeHexObjData } from "@/app/colors/web-safe/data";
 import Link from "next/link";
@@ -9,7 +9,7 @@ import Link from "next/link";
 type WebSafePageClientProps = { data: WebSafeHexObjData[] | undefined | null };
 
 export default function WebSafePageClient({ data = [] }: WebSafePageClientProps) {
-  const [selector, setSelector] = useState(0);
+  const [selector, setSelector] = useState(1);
 
   const dataResult = useMemo((): WebSafeHexObjData[] => {
     try {
@@ -32,13 +32,27 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
       <div>총{dataLength}개의 색상</div>
       <div>
         <div>{dataResultLength}개의 색상</div>
-        {dataResult.map(({ hex: { r, g, b } }, i) => {
-          return (
-            <div key={i} style={{ background: `#${`${r}${g}${b}`}` }}>
-              <Link href={`/colors/web-safe/${r}${g}${b}`}>{`${r}${g}${b}`}</Link>
-            </div>
-          );
-        })}
+        <div className="flex flex-wrap gap-2 content-between justify-start">
+          {dataResult.map(({ hex: { r, g, b } }, i) => {
+            const colorHexText = `${r}${g}${b}`;
+            return (
+              <div
+                key={i}
+                className="max-w-48 min-w-56 w-[80%] aspect-[1/1] rounded-lg pb-2 pl-1 pr-1 flex justify-end flex-col border-white border-solid border-[1px]"
+                style={{ background: `#${colorHexText}` }}
+              >
+                <div className="flex flex-row justify-between content-center bg-[#fff] text-black p-2 rounded-lg border-solid border-black border-[1px]">
+                  <Link href={`/colors/web-safe/${colorHexText}`} title={`#${colorHexText} 색상의 상세보기 페이지로 이동합니다.`}>
+                    #{colorHexText}
+                  </Link>
+                  <button type="button" title={`#${colorHexText} 색상의 값을 복사 합니다.`} onClick={() => alert("테스트 중입니다.")}>
+                    복사
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
