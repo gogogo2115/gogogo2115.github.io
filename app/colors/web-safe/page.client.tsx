b55af116c779e281948a465f988a2c3e4f6d9932
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-01 03:43:32 +0900

    복사하기 기능 부여

--- app/colors/web-safe/page.client.tsx
+++ app/colors/web-safe/page.client.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { notFound } from "next/navigation";
 import { useState, useMemo, MouseEvent } from "react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 import { type WebSafeHexObjData } from "@/app/colors/web-safe/data";
-import Link from "next/link";
+import { copyToClipboard } from "@/utils/copyToClipboard";
 
 type WebSafePageClientProps = { data: WebSafeHexObjData[] | undefined | null };
 
@@ -15,7 +16,8 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
       if (!Array.isArray(data)) throw new Error("자료 타입의 오류 발생");
       if (data.length != 216) throw new Error("자료 개수의 오류 발생");
       if (selector == 0) return data; // 전체 출력
-      if (selector == 1) return data.filter((v) => v.isTrueSafeColor === true); // safest web colors
+      if (selector == 1) return data.filter(({ isTrueSafeColor }) => isTrueSafeColor === true); // safest web colors
+      if (selector == 2) return data.filter(({ hex: { r, g, b } }) => r == g && g == b);
       return data;
     } catch (e) {
       return []; // 오류 [] 처리
@@ -24,7 +26,11 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
 
   const onClickCopyBtn = (e: MouseEvent<HTMLButtonElement>, text: string) => {
     e.preventDefault();
-    alert(text);
+    copyToClipboard(text, {
+      onSuccess: () => alert("복사하였습니다."),
+      onFailure: () => alert("복사 오류"),
+      onNotSupported: () => alert("미지원"),
+    });
     return;
   };
 
@@ -34,8 +40,8 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
   if (dataResultLength <= 0) return notFound(); // 결과값 없음
   return (
     <>
-      <div>총{dataLength}개의 색상</div>
       <div>
+        <div>총{dataLength}개의 색상</div>
         <div>{dataResultLength}개의 색상</div>
         <ul className="grid gap-4 p-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
           {dataResult.map(({ hex: { r, g, b } }, i) => {
@@ -54,6 +60,7 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
 
                 <div className="flex flex-row justify-between content-center align-middle bg-[#fff] text-black pl-1 pr-1 rounded-md">
                   <div style={{ fontWeight: 500 }}>{`#${colorHexText}`}</div>
+
                   <button
                     type="button"
                     aria-label={`#${colorHexText} 색상의 값을 복사 합니다.`}
@@ -61,6 +68,7 @@ export default function WebSafePageClient({ data = [] }: WebSafePageClientProps)
                     onClick={(e) => onClickCopyBtn(e, `#${colorHexText}`)}
                     data-value={`#${colorHexText}`}
                   >
+                    <span className="blind">{`#${colorHexText} 색상의 값을 복사하기`}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                       <path
                         strokeLinecap="round"
