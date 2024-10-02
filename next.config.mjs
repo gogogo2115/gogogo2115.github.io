1615747779487f42c45b07ec2c6bc47c54ca1b24
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-03 04:01:22 +0900

    svg 옵션 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  webpack(config) {
+    // svg 추가
+    config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" }, // svg 추가
+      },
+    },
+  },
+};
 
 export default nextConfig;
