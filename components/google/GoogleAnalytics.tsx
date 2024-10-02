c612e54bb97d81f33cbcaa83f5148b7cfd409fd4
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-03 01:16:46 +0900

    정리 중

--- components/google/GoogleAnalytics.tsx
+++ components/google/GoogleAnalytics.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-import { useEffect } from "react";
 import Script from "next/script";
-
 import useIsomorphicLayoutEffect from "@/hooks/useIsomorphicLayoutEffect";
+import { IS_DEVELOPMENT } from "@/utils/isNodeEnv";
 
-export type GoogleAnalyticsProps = { gaId?: string | null | undefined };
+export type GoogleAnalyticsProps = { gaId?: string | undefined; dataLayerName?: string };
 
-export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
-  //
-  useIsomorphicLayoutEffect(() => {
-    console.log();
-  }, []);
+let currDataLayerName: string | undefined = undefined;
 
-  if (typeof gaId !== "string" || gaId.trim() == "") return null;
+export default function GoogleAnalytics(props: GoogleAnalyticsProps) {
+  const { gaId, dataLayerName = "dataLayer" } = props;
+
+  if (currDataLayerName === undefined) {
+    currDataLayerName = dataLayerName;
+  }
+
+  useIsomorphicLayoutEffect(() => {}, []);
+
+  if (typeof gaId !== "string" || gaId.trim() == "" || IS_DEVELOPMENT) return null;
   return (
     <>
-      <Script id={`ga-init-${gaId}`} />
+      <Script id={`next-ga-init-${gaId}`} />
+      <Script id={`next-ga-{${gaId}}`} src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
     </>
   );
 }
+
+export function sandGAEvent() {}
--- components/google/GoogleTagManager.tsx
+++ components/google/GoogleTagManager.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import { useEffect } from "react";
 import Script from "next/script";
+import useIsomorphicLayoutEffect from "@/hooks/useIsomorphicLayoutEffect";
+import { IS_DEVELOPMENT } from "@/utils/isNodeEnv";
 
-type GoogleTagManagerProps = { gtmId: string | undefined | null };
+type GoogleTagManagerProps = { gtmId?: string | undefined };
 
 export function GoogleTagManager({ gtmId }: GoogleTagManagerProps) {
-  return <></>;
+  //
+  useIsomorphicLayoutEffect(() => {}, []);
+
+  if (typeof gtmId !== "string" || gtmId.trim() == "" || IS_DEVELOPMENT) return null;
+  return (
+    <>
+      <Script id={`gtm-init-${gtmId}`} />
+    </>
+  );
 }
+
+export const sendGTMEvent = (data: Object) => {};
