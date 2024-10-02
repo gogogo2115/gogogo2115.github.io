27cdf1cee513243599c359ec0528f8588f641c5d
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-03 04:35:21 +0900

    일부 속성 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   webpack(config) {
-    // svg 추가
-    config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
+    config.module.rules.push({
+      test: /\.svg$/i, // svg 추가
+      use: ["@svgr/webpack"],
+      // issuer: /\.[jt]sx?$/, // ssr에서 오류 발생으로 주석 처리
+    });
     return config;
   },
   experimental: {
-    turbo: {
-      rules: {
-        "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" }, // svg 추가
-      },
-    },
+    scrollRestoration: true,
+    // turbo: { // github.io 배포 시 작동 여부 불투명 주석 처리
+    //   rules: {
+    //     "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" },
+    //   },
+    // }, // github.io 배포 시 작동 여부 불투명 주석 처리
   },
 };
 
