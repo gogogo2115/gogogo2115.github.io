3954e7851286802f112aed150c78474e378a0557
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-03 19:36:15 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 import bundleAnalyzer from "@next/bundle-analyzer";
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-  openAnalyzer: false,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -16,9 +11,8 @@ const nextConfig = {
       윈도우에서 빌드시 발생 됨 캐시관련 오류 제거
     */
     config.cache = false;
-
     config.module.rules.push({
-      test: /\.svg$/i, // svg 추가
+      test: /\.svg$/i,
       use: ["@svgr/webpack"],
       // issuer: /\.[jt]sx?$/, // ssr에서 오류 발생으로 주석 처리
     });
@@ -26,12 +20,15 @@ const nextConfig = {
   },
   experimental: {
     scrollRestoration: true,
-    // turbo: { // github.io 배포 시 작동 여부 불투명 주석 처리
-    //   rules: {
-    //     "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" },
-    //   },
-    // }, // github.io 배포 시 작동 여부 불투명 주석 처리
+    // turbo: {// github.io 배포 시 작동 여부 불투명 주석 처리
+    //   rules: { "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" } },
+    // },
   },
 };
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
 
 export default withBundleAnalyzer(nextConfig);
