491e0400659763bb18b1ba2582ee97e8d5411f59
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-08 23:46:56 +0900

    Create arrayShuffle.js

---
+++ utils/arrayShuffle.js
@@ -0,0 +1,17 @@
+/**
+ * Knuth Shuffle 방식
+ * @param { array } array 입력한 배열
+ * @returns { array } 섞인 배열 결과값
+ */
+const arrayShuffle = (array) => {
+  if (Array.isArray(array)) {
+    const n = array.length - 1;
+    for (let i = n; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  return [];
+};
+export default arrayShuffle;
