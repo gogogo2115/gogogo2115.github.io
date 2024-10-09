7e3101251b13034f096ccb3a13fa41a1ad094ee7
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-10 00:52:25 +0900

    jsDoc 추가

--- utils/isNodeEnv.js
+++ utils/isNodeEnv.js
@@ -1,4 +1,5 @@
 const NODE_ENV = process.env.NODE_ENV;
+
 /**
  * @type {boolean}
  */
@@ -15,14 +16,16 @@ const IS_DEVELOPMENT = NODE_ENV === "development";
 const IS_TEST = NODE_ENV === "test";
 
 /**
- * @typedef {Object} ISNodeEnv
- * @property {boolean} IS_PRODUCTION
- * @property {boolean} IS_DEVELOPMENT
- * @property {boolean} IS_TEST
+ *  Node.js 실행 환경을 나타내는 플래그 객체의 타입 정의
+ * @typedef {Object} IS_NODE_ENV
+ * @property {boolean} IS_PRODUCTION - 프로덕션 환경 여부
+ * @property {boolean} IS_DEVELOPMENT - 개발 환경 여부
+ * @property {boolean} IS_TEST - 테스트 환경 여부
  */
 
 /**
- * @type {ISNodeEnv}
+ * 현재 Node.js 실행 환경을 나타내는 플래그 객체
+ * @type {IS_NODE_ENV}
  */
 const IS_NODE_ENV = { IS_PRODUCTION, IS_DEVELOPMENT, IS_TEST };
 
