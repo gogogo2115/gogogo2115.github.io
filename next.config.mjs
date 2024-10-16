d1d2f890bbd41eff5f2ec80640d9c4f1278be653
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-16 23:18:08 +0900

    sass 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+import path from "path";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
+const __dirname = path.resolve();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
