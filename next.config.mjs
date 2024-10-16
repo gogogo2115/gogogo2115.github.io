f2887ce87f94c5e86e036aaa7b9fd22ee602811f
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-16 23:29:10 +0900

    svg 관련 진행

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,17 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack(config) {
+    // issuer: /\.[jt]sx?$/, // @svgr/webpack ssr에서 오류 발생으로 제거됨
+    config.module.rules.push({ test: /\.svg$/i, use: ["@svgr/webpack"] });
+    return config;
+  },
+  experimental: {
+    scrollRestoration: true,
+    // turbo: {// github.io 배포 시 작동 여부 불투명 주석 처리
+    //   rules: { "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" } },
+    // },
+  },
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
