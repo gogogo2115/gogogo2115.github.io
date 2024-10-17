70279922888954ec10459797ac4f16a24c959c99
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-18 02:32:26 +0900

    빌드 정보 추가

--- utils/config.env.js
+++ utils/config.env.js
@@ -2,9 +2,12 @@
 "use strict";
 
 const packageJSON = require("../package.json");
+const NEXT_VERSION = (packageJSON.dependencies.next || "").replace(/[\^~]/g, ""); // next.js 버전
 
-const NEXT_VERSION = (packageJSON.dependencies.next || "").replace(/[\^~]/g, "");
+const buildDate = new Date(); // 마지막 빌드일
+const BUILD_ISO = buildDate.toISOString();
+const BUILD_TIMESTAMP = buildDate.getTime().toString();
 
-const env = { NEXT_VERSION };
+const env = { NEXT_VERSION, BUILD_ISO, BUILD_TIMESTAMP };
 
 module.exports = env;
