b874cd061676df7bf0bd87f31607ba2f470919a4
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-20 01:29:59 +0900

    Update page.tsx

--- app/colors/contrast/page.tsx
+++ app/colors/contrast/page.tsx
@@ -2,10 +2,10 @@ import ContrastPageClient from "@/app/colors/contrast/page.client";
 import { randHexObj } from "@/utils/color";
 
 // import dynamic from "next/dynamic";
-// const DynamicComponentWithNoSSR = dynamic(() => import("./page.client"), { ssr: false });
+// const ContrastPageClient = dynamic(() => import("./page.client"), { ssr: false });
 
 export default function ContrastPage() {
-  const fontHexObj = randHexObj();
-  const backHexObj = randHexObj();
+  const fontHexObj = randHexObj(),
+    backHexObj = randHexObj();
   return <ContrastPageClient fontHexObj={fontHexObj} backHexObj={backHexObj} />;
 }
