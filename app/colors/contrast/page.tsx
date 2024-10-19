4444d4caa1aa634c722209aa70d772c531b4814e
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-19 22:49:15 +0900

    Update page.tsx

--- app/colors/contrast/page.tsx
+++ app/colors/contrast/page.tsx
@@ -1,5 +1,8 @@
 import ContrastPageClient from "@/app/colors/contrast/page.client";
+import { randHexObj } from "@/utils/color";
 
 export default function ContrastPage() {
-  return <ContrastPageClient />;
+  const fontHexObj = randHexObj();
+  const backHexObj = randHexObj();
+  return <ContrastPageClient fontHexObj={fontHexObj} backHexObj={backHexObj} />;
 }
