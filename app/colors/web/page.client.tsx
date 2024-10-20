e8286d17325fb5733fab1993d79dbf291389e42a
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-21 04:20:17 +0900

    시멘틱 태그 와 메타 태그 추가

--- app/colors/web/page.client.tsx
+++ app/colors/web/page.client.tsx
@@ -1,3 +1,8 @@
 export default function WebPageClient() {
-  return <></>;
+  return (
+    <>
+      <h1>웹 색상</h1>
+      <h2>웹 색상 리스트</h2>
+    </>
+  );
 }
--- app/colors/web/page.tsx
+++ app/colors/web/page.tsx
@@ -1,5 +1,12 @@
 import WebPageClient from "@/app/colors/web/page.client";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "웹 색상",
+  description: "웹 색상",
+};
+
 export default function WebPage() {
   return <WebPageClient />;
 }
