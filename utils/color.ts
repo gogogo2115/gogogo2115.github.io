ee9e678def052384e9fa455dcb54bcabc7be78b9
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-20 20:58:52 +0900

    함수 추가

--- utils/color.ts
+++ utils/color.ts
@@ -30,6 +30,7 @@ export type OKLCH_OBJ = { l: number; c: number; h: number };
 export const clamp = (value: number, min: number = 0, max: number = 0, defaultValue: number = 0): number => {
   // if (min === undefined) min = -Infinity;
   // if (max === undefined) max = Infinity;
+  if (min === max) return min;
   const [low, high] = min < max ? [min, max] : [max, min];
   const calc = Math.max(low, Math.min(high, value));
   return isNaN(calc) ? defaultValue : calc;
@@ -54,3 +55,15 @@ export const randRgbObj = (): RGB_OBJ => ({ r: randRgb0to255(), g: randRgb0to255
 
 export const randHex0to255 = (): string => randRgb0to255().toString(16).padStart(2, "0").toUpperCase();
 export const randHexObj = (): HEX_OBJ => ({ r: randHex0to255(), g: randHex0to255(), b: randHex0to255() });
+
+export const isValidHexObj = (hexObj: Partial<HEX_OBJ>): hexObj is HEX_OBJ => {
+  const regex = /^[0-9A-Fa-f]{1,2}$/i;
+  const { r, g, b } = hexObj;
+  return typeof r === "string" && regex.test(r) && typeof g === "string" && regex.test(g) && typeof b === "string" && regex.test(b);
+};
+
+export const isValidRgbObj = (rgbObj: Partial<RGB_OBJ>): rgbObj is RGB_OBJ => {
+  const { r, g, b } = rgbObj;
+  if (typeof r !== "number" || typeof g !== "number" || typeof b !== "number") return false;
+  return Number.isInteger(r) && r >= 0 && r <= 255 && Number.isInteger(g) && g >= 0 && g <= 255 && Number.isInteger(b) && b >= 0 && b <= 255;
+};
