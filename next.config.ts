e3ca8cde6fae6fe91e9703a27d8c3aa20145b21e
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-23 21:17:34 +0900

    @next/bundle-analyzer 와 sass 설정 반영

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+import path from "path";
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
+
+export default withBundleAnalyzer(nextConfig);
