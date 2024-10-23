7d4ef562dca8ff0e546b429e0c204106677c70cf
borland <46901223+gogogo2115@users.noreply.github.com>
2024-10-23 22:38:40 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,24 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: false, // IS_DEVELOPMENT,
+  poweredByHeader: false, // IS_DEVELOPMENT,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    implementation: "sass-embedded",
+  },
+  webpack(config) {
+    // issuer: /\.[jt]sx?$/, // @svgr/webpack ssr에서 오류 발생으로 제거됨
+    config.module.rules.push({ test: /\.svg$/i, use: ["@svgr/webpack"] });
+    return config;
   },
   experimental: {
-    scrollRestoration: true,
+    turbo: {
+      rules: {
+        "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" },
+      },
+    },
   },
 };
 
