58a506c4e9bd91b558870f3cba3689040c147a65
borland <46901223+gogogo2115@users.noreply.github.com>
2025-03-12 21:51:19 +0900

    특정 환경 변수 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import ENV_CONFIG from "./utils/envConfig";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: ENV_CONFIG,
 };
 
 export default nextConfig;
