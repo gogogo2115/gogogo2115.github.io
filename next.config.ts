b383ded5d0f659d17db4a223c172941488fcc5b4
borland <46901223+gogogo2115@users.noreply.github.com>
2025-03-15 03:24:36 +0900

    @next/bundle-analyzer 설치

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
 import ENV_CONFIG from "./utils/envConfig";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   env: ENV_CONFIG,
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+});
+
+export default withBundleAnalyzer(nextConfig);
