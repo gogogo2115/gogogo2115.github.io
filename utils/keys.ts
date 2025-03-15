1211b2233d220d63ad50319621a6647d003ff5b4
borland <46901223+gogogo2115@users.noreply.github.com>
2025-03-16 00:30:40 +0900

    Create keys.ts

---
+++ utils/keys.ts
@@ -0,0 +1,9 @@
+export const buildPublicKey = ((): string => {
+  const key = (process.env.BUILD_PUBLIC_KEY ?? "").trim();
+  return key;
+})();
+
+export const buildPrivateKey = ((): string => {
+  const key = (typeof window !== "undefined" ? process.env.BUILD_PRIVATE_KEY ?? "" : "").trim();
+  return key;
+})();
