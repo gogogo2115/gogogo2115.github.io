6e206a7b5425e6baafc794fe753fc994c25a351b
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-11 19:45:29 +0900

    Update settingsUtils.ts

--- lib/store/slices/settings/settingsUtils.ts
+++ lib/store/slices/settings/settingsUtils.ts
@@ -1,6 +1,9 @@
+export type Theme = "dark" | "light" | "system" | "gray";
+export type FontSize = 1 | 2 | 3 | 4 | 5 | 6;
+export type SettingsState = { theme: Theme; fontSize: FontSize };
+
 export const getMatchMediaTheme = (): "dark" | "light" => {
   if (typeof window === "undefined" || !("matchMedia" in window)) return "light";
-  const query = "(prefers-color-scheme: dark)";
-  const { matches } = window.matchMedia(query);
+  const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
   return matches ? "dark" : "light";
 };
