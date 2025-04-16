40b6215087fd63a4da73100f8f461ab5c346bab4
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-17 00:55:48 +0900

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import { useAppDispatch, useAppSelector } from "@/lib/store";
-
 export default function SettingsPage() {
-  const settings = useAppSelector((state) => state);
-  const dispatch = useAppDispatch();
-
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  return <></>;
 }
