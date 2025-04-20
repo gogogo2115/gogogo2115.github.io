cb7ac852587bbcf50c1d967e4061050edae96443
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-21 05:26:40 +0900

    sassOptions 옵션 부여

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import { CONFIG_ENV } from "@/utils/configEnv";
 const nextConfig: NextConfig = {
   output: "export",
   env: CONFIG_ENV,
+  sassOptions: {
+    implementation: "sass-embedded",
+  },
 };
 
 export default nextConfig;
