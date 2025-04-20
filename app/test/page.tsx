00410b5316241fd0905d3f7cc3adccbf193c9aac
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-21 04:38:20 +0900

    env 보안 관련 진행

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,10 @@
-const buildDate1 = (process.env.BUILD_DATE_ISO ?? "호출 실패").trim();
-const buildDate2 = (
-  process.env.NEXT_PUBLIC_BUILD_DATE_ISO ?? "호출 실패"
-).trim();
+const buildDate = (process.env.BUILD_DATE_ISO ?? "호출 실패").trim();
 
 export default function TestPage() {
   return (
     <>
       <div>{process.env.NODE_ENV}</div>
-      <div>buildDate1: {buildDate1}</div>
-      <div>buildDate2: {buildDate2}</div>
+      <div>buildDate: {buildDate}</div>
     </>
   );
 }
