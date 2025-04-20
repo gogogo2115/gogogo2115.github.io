29c57f8b049f8bc377e889bdb977e4c20c3574dc
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-21 05:04:04 +0900

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,19 @@
-const buildDate = (process.env.BUILD_DATE_ISO ?? "호출 실패").trim();
-const buildRand = (process.env.BUILD_RAND_KEY ?? "호출 실패").trim();
+const BUILD_DATE_ISO = (process.env.BUILD_DATE_ISO ?? "호출 실패").trim();
+const BUILD_RAND_KEY = (process.env.BUILD_RAND_KEY ?? "호출 실패").trim();
+
+const PACKAGE_NAME = (process.env.NEXT_PUBLIC_CONFIG_PACKAGE_NAME ?? "호출 실패").trim();
+const PACKAGE_VERSION = (process.env.NEXT_PUBLIC_CONFIG_PACKAGE_VERSION ?? "호출 실패").trim();
+const NEXT_VERSION = (process.env.NEXT_PUBLIC_CONFIG_NEXT_VERSION ?? "호출 실패").trim();
 
 export default function TestPage() {
   return (
     <>
       <div>{process.env.NODE_ENV}</div>
-      <div>buildDate: {buildDate}</div>
-      <div>buildRand: {buildRand}</div>
-      <div>{process.env.NEXT_PUBLIC_CONFIG_PACKAGE_NAME}</div>
-      <div>{process.env.NEXT_PUBLIC_CONFIG_PACKAGE_VERSION}</div>
-      <div>{process.env.NEXT_PUBLIC_CONFIG_NEXT_VERSION}</div>
+      <div>BUILD_DATE_ISO: {BUILD_DATE_ISO}</div>
+      <div>BUILD_RAND_KEY: {BUILD_RAND_KEY}</div>
+      <div>PACKAGE_NAME: {PACKAGE_NAME}</div>
+      <div>PACKAGE_VERSION: {PACKAGE_VERSION}</div>
+      <div>NEXT_VERSION: {NEXT_VERSION}</div>
     </>
   );
 }
