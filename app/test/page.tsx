d6599e285c0ce396b986dce43dd35d05cf41ab05
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-21 04:58:25 +0900

    테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,5 @@
 const buildDate = (process.env.BUILD_DATE_ISO ?? "호출 실패").trim();
 const buildRand = (process.env.BUILD_RAND_KEY ?? "호출 실패").trim();
-const buildPublic = (process.env.BUILD_PUBLIC_KEY ?? "호출 실패").trim();
-const buildPrivate = (process.env.BUILD_PRIVATE_KEY ?? "호출 실패").trim();
 
 export default function TestPage() {
   return (
@@ -9,8 +7,9 @@ export default function TestPage() {
       <div>{process.env.NODE_ENV}</div>
       <div>buildDate: {buildDate}</div>
       <div>buildRand: {buildRand}</div>
-      <div>buildPublic: {buildPublic}</div>
-      <div>buildPrivate: {buildPrivate}</div>
+      <div>{process.env.NEXT_PUBLIC_CONFIG_PACKAGE_NAME}</div>
+      <div>{process.env.NEXT_PUBLIC_CONFIG_PACKAGE_VERSION}</div>
+      <div>{process.env.NEXT_PUBLIC_CONFIG_NEXT_VERSION}</div>
     </>
   );
 }
