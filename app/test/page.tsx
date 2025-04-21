b04382ba5373abe5f0b0219ecf70ac2e594a5b41
borland <46901223+gogogo2115@users.noreply.github.com>
2025-04-21 15:44:05 +0900

    테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,9 +5,12 @@ const PACKAGE_NAME = (process.env.NEXT_PUBLIC_CONFIG_PACKAGE_NAME ?? "호출 실
 const PACKAGE_VERSION = (process.env.NEXT_PUBLIC_CONFIG_PACKAGE_VERSION ?? "호출 실패").trim();
 const NEXT_VERSION = (process.env.NEXT_PUBLIC_CONFIG_NEXT_VERSION ?? "호출 실패").trim();
 
+import SVG from "@/public/next.svg";
+
 export default function TestPage() {
   return (
     <>
+      <SVG />
       <div>{process.env.NODE_ENV}</div>
       <div>BUILD_DATE_ISO: {BUILD_DATE_ISO}</div>
       <div>BUILD_RAND_KEY: {BUILD_RAND_KEY}</div>
