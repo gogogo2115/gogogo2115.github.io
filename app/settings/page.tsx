d8090800c9ac935c989b60beb7f38d32192b67da
borland <46901223+gogogo2115@users.noreply.github.com>
2025-05-02 15:47:11 +0900

    테스트 업로드

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useAppDispatch } from "@/lib/store";
+import { setTheme } from "@/lib/store/slice/settings/settingsSlice";
+import { isValidTheme } from "@/lib/store/slice/settings/settingsUtils";
+import { MouseEvent } from "react";
+
 export default function SettingsPage() {
-  return <></>;
+  const dispatch = useAppDispatch();
+
+  const handleThemeChange = (ev: MouseEvent<HTMLButtonElement>) => {
+    ev.preventDefault();
+    const target = ev.currentTarget;
+    const theme = target.dataset["theme"] ?? "";
+    if (isValidTheme(theme)) {
+      dispatch(setTheme(theme));
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleThemeChange} data-theme="dark">
+        dark
+      </button>
+      /
+      <button onClick={handleThemeChange} data-theme="light">
+        light
+      </button>
+      /
+      <button onClick={handleThemeChange} data-theme="system">
+        system
+      </button>
+    </div>
+  );
 }
