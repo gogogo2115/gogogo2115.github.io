411df53098997d0b90413d242093a91190ce8428
borland <46901223+gogogo2115@users.noreply.github.com>
2025-05-21 01:46:55 +0900

    Update settingsSlice.ts

--- lib/store/slice/settings/settingsSlice.ts
+++ lib/store/slice/settings/settingsSlice.ts
@@ -1,26 +1,58 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { clampTheme, clampFontSize, DEFAULT_THEME, DEFAULT_FONT_SIZE, NAME, InitialState, Theme } from "./settingsUtils";
+import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
+import { clampTheme, clampFontSize, DEFAULT_THEME, DEFAULT_FONT_SIZE, Theme, FontSize, updateDocument, saveStorage, DEFAULT_KEY_NAME, SettingsState } from "./settingsUtils";
 
-export const getInitialState = (): InitialState => {
+export const getInitialState = (): SettingsState => {
   try {
     if (typeof window === "undefined" || !("localStorage" in window)) throw new Error(`localStorage를 지원하지 않거나 사용이 불가능한 환경입니다.`);
-    const settings = (window.localStorage.getItem(NAME) ?? "").trim();
+    const settings = (window.localStorage.getItem(DEFAULT_KEY_NAME) ?? "").trim();
     if (!settings) throw new Error(`localStorage ${name}값이 존재하지 않습니다.`);
-    const parsed = JSON.parse(settings) as Partial<InitialState>;
-    return { theme: clampTheme(parsed.theme), fontSize: clampFontSize(parsed.fontSize) };
+    const parsed = JSON.parse(settings) as Partial<SettingsState>;
+    const state = { theme: clampTheme(parsed.theme), fontSize: clampFontSize(parsed.fontSize) };
+    return state;
   } catch {
     return { theme: DEFAULT_THEME, fontSize: DEFAULT_FONT_SIZE };
   }
 };
 
 export const settingsSlice = createSlice({
-  name: NAME,
+  name: DEFAULT_KEY_NAME,
   initialState: getInitialState(),
   reducers: (create) => ({
+    setInitialState: create.reducer((state) => {
+      const getState = getInitialState();
+      state.theme = getState.theme;
+      state.fontSize = getState.fontSize;
+      const currentState = current(state);
+      updateDocument(currentState);
+      saveStorage(currentState);
+    }),
     setTheme: create.reducer((state, actions: PayloadAction<Theme>) => {
       const actionTheme = clampTheme(actions.payload);
       state.theme = actionTheme;
-      document.body.setAttribute("data-theme", actionTheme);
+      const currentState = current(state);
+      updateDocument(currentState);
+      saveStorage(currentState);
+    }),
+    setFontSize: create.reducer((state, actions: PayloadAction<FontSize>) => {
+      const actionFontSize = clampFontSize(actions.payload);
+      state.fontSize = actionFontSize;
+      const currentState = current(state);
+      updateDocument(currentState);
+      saveStorage(currentState);
+    }),
+    setIncrementFontSize: create.reducer((state) => {
+      const stateFontSize = clampFontSize(state.fontSize + 1);
+      state.fontSize = stateFontSize;
+      const currentState = current(state);
+      updateDocument(currentState);
+      saveStorage(currentState);
+    }),
+    setDecrementFontSize: create.reducer((state) => {
+      const stateFontSize = clampFontSize(state.fontSize - 1);
+      state.fontSize = stateFontSize;
+      const currentState = current(state);
+      updateDocument(currentState);
+      saveStorage(currentState);
     }),
   }),
   selectors: {
@@ -30,3 +62,20 @@ export const settingsSlice = createSlice({
 
 export const { selectStoreSettings } = settingsSlice.selectors;
 export const { setTheme } = settingsSlice.actions;
+
+// setSettingsTheme: create.reducer((state, actions: PayloadAction<Theme>) => {
+//   const actionTheme = clampTheme(actions.payload);
+//   state.theme = actionTheme;
+// }),
+// setSettingsFontSize: create.reducer((state, actions: PayloadAction<FontSize>) => {
+//   const actionFontSize = clampFontSize(actions.payload);
+//   state.fontSize = actionFontSize;
+// }),
+// setSettingsIncrementFontSize: create.reducer((state) => {
+//   const fontSize = clampFontSize(state.fontSize + 1);
+//   state.fontSize = fontSize;
+// }),
+// setSettingsDecrementFontSize: create.reducer((state) => {
+//   const fontSize = clampFontSize(state.fontSize - 1);
+//   state.fontSize = fontSize;
+// }),
