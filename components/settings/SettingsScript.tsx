db2284dec73aabe9b97059fc3fcafb7a86c49157
borland <46901223+gogogo2115@users.noreply.github.com>
2025-05-22 20:45:08 +0900

    기본 값 적용

--- components/settings/SettingsScript.tsx
+++ components/settings/SettingsScript.tsx
@@ -1,6 +1,40 @@
 /* eslint-disable @next/next/no-sync-scripts */
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/lib/store";
+import { selectSettings } from "@/lib/store/slice/settings/settingsSlice";
+import { DEFAULT_KEY_NAME } from "@/lib/store/slice/settings/settingsUtils";
+import { useLayoutEffect } from "react";
+
 export function SettingsScript() {
-  return <script key="SettingsScript" id="SettingsScript" src="/settingsScript.js" />;
+  const settings = useAppSelector(selectSettings);
+  const dispatch = useAppDispatch();
+
+  useLayoutEffect(() => {
+    if (typeof window === "undefined" || !("localStorage" in window)) return;
+
+    const handleStorageChange = (ev: StorageEvent) => {
+      if (ev.key !== DEFAULT_KEY_NAME) return;
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (typeof window === "undefined" || !("matchMedia" in window) || settings.theme !== "system") return;
+
+    const handleMediaQueryChange = (ev: MediaQueryListEvent) => {
+      const systemTheme = ev.matches ? "dark" : "light";
+      document.documentElement.setAttribute("data-theme", systemTheme);
+      document.documentElement.style.colorScheme = systemTheme;
+    };
+
+    const query = "(prefers-color-scheme: dark)";
+    const mediaQuery = window.matchMedia(query);
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
+  }, [settings.theme]);
+
+  return <script key="SettingsScript" id="SettingsScript" dangerouslySetInnerHTML={{ __html: `` }} />;
 }
