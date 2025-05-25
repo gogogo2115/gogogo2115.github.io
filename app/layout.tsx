fab256dca6dc69853c43b4d48bf6b4645cd8a2f7
borland <46901223+gogogo2115@users.noreply.github.com>
2025-05-25 22:01:52 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,6 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 const NEXT_VERSION = `Next.js ${process.env.NEXT_PUBLIC_CONFIG_NEXT_VERSION ?? ""}`.trim();
-// const BUILD_RAND_KEY = (process.env.BUILD_RAND_KEY ?? "unknown").replace(/\s+/g, "");
 
 export const metadata: Metadata = {
   title: "gogogo2115.github.io",
