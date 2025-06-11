c2cb1420ae435c10ab6fece8f4940915120d5f04
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-11 18:08:54 +0900

    env 관련 추가

---
+++ types/env.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_PACKAGE_VERSION: string;
+    NEXT_PUBLIC_PACKAGE_NAME: string;
+    NEXT_PUBLIC_NEXT_VERSION: string;
+    NEXT_PUBLIC_REACT_VERSION: string;
+
+    BUILD_DATE_ISO: string;
+    BUILD_RAND_KEY: string;
+    // BUILD_PUBLIC_KEY 숨김 처리 (서버에서만 호출)
+    // BUILD_PRIVATE_KEY 숨김 처리 (서버에서만 호출)
+    // Add other environment variables here
+  }
+}
--- utils/buildKeys.ts
+++ utils/buildKeys.ts
@@ -2,7 +2,8 @@ export const BUILD_PUBLIC_KEY = (): string | undefined => {
   return process.env.BUILD_PUBLIC_KEY;
 };
 
+// 노출 방지 (서버에서만 호출되도록 처리)
 export const BUILD_PRIVATE_KEY = (): string | undefined => {
-  if (typeof window === "undefined") return process.env.BUILD_PUBLIC_KEY;
+  if (typeof window === "undefined") return process.env.BUILD_PRIVATE_KEY;
   return undefined;
 };
