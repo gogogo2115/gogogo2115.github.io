9831f4c107adba36335c8dff84f681f5efaa0517
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-12 03:35:10 +0900

    rules 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  // baseDirectory: import.meta.dirname,
   baseDirectory: __dirname,
 });
 
+// 기본 값
+// const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // 사용하지 않는 변수를 warn 처리
+      "react/no-unescaped-entities": "off", // 이스케이프 처리 않은 특수문자를 그대로 사용할 수 있도록 허용(off)
+    },
+  }),
 ];
 
 export default eslintConfig;
