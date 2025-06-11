1e26bf642d6428816a8ebf2d01969517415e194a
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-11 18:29:11 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import { CONFIG_ENV } from "./utils/configEnv";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  output: "export",
   env: CONFIG_ENV,
   sassOptions: {
     implementation: "sass-embedded",
