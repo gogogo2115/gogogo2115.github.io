7148a8e99948fb957d45e3bc4891aff3777ab6b3
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-15 04:23:19 +0900

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,21 +1,3 @@
-import { privateDecrypt, publicEncrypt } from "crypto";
-
-const validatePrivateKey = (privateKey: string): boolean => {
-  try {
-    if (typeof window !== "undefined") throw new Error("서버환경에서만 실행 가능합니다.");
-
-    const testMessage = "ValidationTest";
-    const publicKey = process.env.BUILD_PUBLIC_KEY;
-
-    const encrypted = publicEncrypt(publicKey, Buffer.from(testMessage));
-    const decrypted = privateDecrypt(privateKey, encrypted).toString("utf-8");
-
-    return testMessage === decrypted;
-  } catch {
-    return false;
-  }
-};
-
 export default function TestPage() {
   const BUILD_DATE_ISO = process.env.BUILD_DATE_ISO ?? "";
   const BUILD_RAND_KEY = process.env.BUILD_RAND_KEY ?? "";
@@ -23,10 +5,6 @@ export default function TestPage() {
     <>
       <div>BUILD_DATE_ISO: {BUILD_DATE_ISO}</div>
       <div>BUILD_RAND_KEY: {BUILD_RAND_KEY}</div>
-      <div>{process.env.BUILD_PUBLIC_KEY}</div>
-      <div>{process.env.BUILD_PRIVATE_KEY}</div>
-
-      <div>validatePrivateKey: {validatePrivateKey(process.env.BUILD_PRIVATE_KEY ?? "") ? "정상" : "비정상"}</div>
     </>
   );
 }
