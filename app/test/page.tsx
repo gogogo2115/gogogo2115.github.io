d7150c36a1316ba28d2d5fed7646ddbbd726351b
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-16 01:41:42 +0900

    svg 테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import Svg from "@/public/next.svg";
+
 export default function TestPage() {
   const BUILD_DATE_ISO = process.env.BUILD_DATE_ISO ?? "";
   const BUILD_RAND_KEY = process.env.BUILD_RAND_KEY ?? "";
   return (
     <>
+      <Svg />
       <div>BUILD_DATE_ISO: {BUILD_DATE_ISO}</div>
       <div>BUILD_RAND_KEY: {BUILD_RAND_KEY}</div>
     </>
