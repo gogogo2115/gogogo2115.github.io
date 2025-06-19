3a2e5be315d790b8697ed72bb33be9d88dd0587b
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-19 21:51:09 +0900

    패키지 업데이트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import TestPageClient from "./page.client";
 
 export const metadata: Metadata = {
   title: "테스트 정보",
@@ -6,12 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function TestPage() {
-  const BUILD_DATE_ISO = process.env.BUILD_DATE_ISO ?? "";
-  const BUILD_RAND_KEY = process.env.BUILD_RAND_KEY ?? "";
-  return (
-    <>
-      <div>BUILD_DATE_ISO: {BUILD_DATE_ISO}</div>
-      <div>BUILD_RAND_KEY: {BUILD_RAND_KEY}</div>
-    </>
-  );
+  return <TestPageClient />;
 }
