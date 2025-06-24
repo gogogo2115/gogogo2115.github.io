4c7c9285bc543dc8a9c65e9935fa07a21d4c6488
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-25 02:04:56 +0900

    Update buildKeys.ts

--- utils/buildKeys.ts
+++ utils/buildKeys.ts
@@ -1,36 +1,33 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 
 // 노출 방지 (서버에서만 호출되도록 처리)
-const isClientCache = typeof window !== "undefined";
+const isServerCache = typeof window === "undefined";
 
-export const GET_BUILD_PRIVATE_KEY = (): string | undefined => {
-  if (isClientCache) return undefined;
-  const pk = process.env.BUILD_PRIVATE_KEY;
-  return typeof pk === "string" ? pk : undefined;
-};
+export const GET_BUILD_PUBLIC_KEY: string = process.env.BUILD_PUBLIC_KEY;
 
-let privateKeyFileCache: string | undefined;
-let cacheInitialized: boolean = false;
-export const GET_BUILD_PRIVATE_KEY_FILE = (): string | undefined => {
-  if (isClientCache) return undefined;
-  if (cacheInitialized) return privateKeyFileCache;
-  try {
-    // Node.js 모듈 타입 정의
-    const fs = require("fs") as typeof import("fs");
-    const path = require("path") as typeof import("path");
+export const GET_BUILD_PRIVATE_KEY: string | undefined = isServerCache ? process.env.BUILD_PRIVATE_KEY : undefined;
 
-    const privateKeyPath = path.join(process.cwd(), "keys", "buildPrivateKey.pem");
-    if (!fs.existsSync(privateKeyPath)) {
-      privateKeyFileCache = undefined;
-    } else {
-      const readFile = fs.readFileSync(privateKeyPath, "utf-8").trim();
-      privateKeyFileCache = typeof readFile === "string" ? readFile : undefined;
-    }
-  } catch {
-    privateKeyFileCache = undefined;
-  } finally {
-    cacheInitialized = true;
-  }
+// let cacheInitialized: boolean = false;
+// let privateKeyFileCache: string | undefined;
+// export const GET_BUILD_PRIVATE_KEY_FILE = ((): string | undefined => {
+//   if (!isServerCache) return undefined;
+//   if (cacheInitialized) return privateKeyFileCache;
+//   try {
+//     // Node.js 모듈 타입 정의
+//     const fs = require("fs") as typeof import("fs");
+//     const path = require("path") as typeof import("path");
 
-  return privateKeyFileCache;
-};
+//     const privateKeyPath = path.join(process.cwd(), "keys", "buildPrivateKey.pem");
+//     if (!fs.existsSync(privateKeyPath)) {
+//       privateKeyFileCache = undefined;
+//     } else {
+//       const readFile = fs.readFileSync(privateKeyPath, "utf-8").trim();
+//       privateKeyFileCache = readFile;
+//     }
+//   } catch {
+//     privateKeyFileCache = undefined;
+//   } finally {
+//     cacheInitialized = true;
+//   }
+//   return privateKeyFileCache;
+// })();
