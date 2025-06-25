e42c6f55089fbd091c0371495a8705c1b64f8397
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-26 03:02:32 +0900

    명칭 변경

--- hooks/useIsClient.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-export const useIsClient = () => {
-  const [isClient, setClient] = useState<boolean>(false);
-  useEffect(() => setClient(true), []);
-  return isClient;
-};
