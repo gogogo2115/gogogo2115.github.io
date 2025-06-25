2ffbcfa9b867a3c892f58e0b922f58d4ebdbf72c
borland <46901223+gogogo2115@users.noreply.github.com>
2025-06-26 00:27:21 +0900

    Update useIsClient.ts

--- hooks/useIsClient.ts
+++ hooks/useIsClient.ts
@@ -4,10 +4,6 @@ import { useEffect, useState } from "react";
 
 export const useIsClient = () => {
   const [isClient, setClient] = useState<boolean>(false);
-
-  useEffect(() => {
-    setClient(true);
-  }, []);
-
+  useEffect(() => setClient(true), []);
   return isClient;
 };
