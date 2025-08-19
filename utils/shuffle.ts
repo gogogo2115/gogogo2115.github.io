a55a728cbc7ca901710e6ccad92378948fea425c
borland <46901223+gogogo2115@users.noreply.github.com>
2025-08-19 21:05:25 +0900

    배열 출력 오류 수정

--- utils/shuffle.ts
+++ utils/shuffle.ts
@@ -2,7 +2,7 @@ export const arrayShuffle = <T>(arr: T[]): T[] => {
   if (!Array.isArray(arr)) return [];
   const copyArr = [...arr]; // 원본 배열을 변경하지 않도록 복사
   const n = copyArr.length - 1;
-  for (let i = n - 1; i > 0; i--) {
+  for (let i = n; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [copyArr[i], copyArr[j]] = [copyArr[j], copyArr[i]];
   }
