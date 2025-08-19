8920cf0fe8aacff9d295cebf2119d6df4c822024
borland <46901223+gogogo2115@users.noreply.github.com>
2025-08-19 20:55:32 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
-import { IS_DEVELOPMENT, IS_PRODUCTION } from "@/utils/configNode";
+import { IS_PRODUCTION } from "@/utils/configNode";
 import { CONFIG_ENV } from "./utils/configEnv";
 
 const nextConfig: NextConfig = {
   /* config options here */
   generateBuildId: async () => `build-${CONFIG_ENV.BUILD_RAND_KEY.replace(/[_]/g, "-")}`,
-  reactStrictMode: IS_DEVELOPMENT,
+  reactStrictMode: false,
   compress: true,
   output: "export",
   env: CONFIG_ENV,
