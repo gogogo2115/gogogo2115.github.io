9e485cf74530b5c9949c5211ca28f711233e4546
borland <46901223+gogogo2115@users.noreply.github.com>
2025-08-20 23:00:49 +0900

    Update page.tsx

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,9 +1,9 @@
 export default function DevPage() {
   return (
     <div>
-      <div>next version: {process.env.NEXT_PUBLIC_NEXT_VERSION}</div>
-      <div>react version: {process.env.NEXT_PUBLIC_REACT_VERSION}</div>
-      <div>build iso: {process.env.BUILD_DATE_ISO}</div>
+      <div>next version: {process.env.NEXT_PUBLIC_NEXT_VERSION ?? "정보 없음"}</div>
+      <div>react version: {process.env.NEXT_PUBLIC_REACT_VERSION ?? "정보 없음"}</div>
+      <div>build iso: {process.env.BUILD_DATE_ISO ?? "정보 없음"}</div>
     </div>
   );
 }
