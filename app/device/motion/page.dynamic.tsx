61d651594700456d16792833e5d6fcd62424dde4
borland <46901223+gogogo2115@users.noreply.github.com>
2025-11-15 02:15:52 +0900

    자 가보자

--- app/device/motion/page.dynamic.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const DynamicDeviceMotion = dynamic(() => import("./page.client"), { ssr: false, loading: () => <div>loading</div> });
-export default DynamicDeviceMotion;
--- hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useEffect, useLayoutEffect } from "react";
-
-const isClient = () => typeof window !== "undefined";
-
-export const useIsomorphicLayoutEffect = isClient() ? useLayoutEffect : useEffect;
