037120f5ab6f875d2e382e54349fd52b26fa7004
borland <gogogo2115@gmail.com>
2025-11-28 04:25:23 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactCompiler: true,
+  webpack: (config) => {
+    config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
+    return config;
+  },
+  turbopack: {
+    rules: {
+      "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" },
+    },
+  },
 };
 
 export default nextConfig;
