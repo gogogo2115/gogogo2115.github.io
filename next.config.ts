b9bd53189cd8635f14859d1b11ebca3e002e2d17
borland <gogogo2115@gmail.com>
2025-11-28 16:46:14 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
+import { IS_DEVELOPMENT, IS_PRODUCTION } from "./utils/env";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactCompiler: true,
+  compiler: {
+    removeConsole: IS_PRODUCTION,
+  },
   webpack: (config) => {
     config.module.rules.push({ test: /\.svg$/i, issuer: /\.[jt]sx?$/, use: ["@svgr/webpack"] });
     return config;
   },
   turbopack: {
+    debugIds: IS_DEVELOPMENT,
     rules: {
       "*.svg": { loaders: ["@svgr/webpack"], as: "*.js" },
     },
