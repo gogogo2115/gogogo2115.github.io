207ee03add22875d8c010deca809f0565a4c4d52
borland <46901223+gogogo2115@users.noreply.github.com>
2026-01-26 00:30:07 +0900

    Update env.config.ts

--- utils/env.config.ts
+++ utils/env.config.ts
@@ -1,21 +1,7 @@
-"use server";
+const NODE_ENV = process.env.NODE_ENV;
 
-const isClient = typeof window !== "undefined";
-if (isClient) throw new Error("env.config: 보안상 서버환경에서만 실행이 가능합니다.");
+export const IS_PRODUCTION = NODE_ENV === "production";
+export const IS_DEVELOPMENT = NODE_ENV === "development";
+export const IS_TEST = NODE_ENV === "test";
 
-const BUILD_DATE_ISO = new Date().toISOString();
-
-const BUILD_RAND_KEY = (() => {
-  try {
-    if (isClient) throw new Error("보안상 서버환경에서만 실행이 가능합니다.");
-    return "";
-  } catch (e) {
-    const msg = e instanceof Error && typeof e.message ? e.message : "알 수 업는 오류";
-    throw new Error(`BUILD_RAND_KEY: ${msg}`);
-  }
-})();
-
-export const ENV_CONFIG = {
-  BUILD_DATE_ISO,
-  BUILD_RAND_KEY,
-};
+export const ENV_CONFIG = { NODE_ENV, IS_PRODUCTION, IS_DEVELOPMENT, IS_TEST };
