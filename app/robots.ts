3ac5610f775d14b0ac2dea82f4b502d8d8d342c1
borland <46901223+gogogo2115@users.noreply.github.com>
2026-01-29 12:15:22 +0900

    dynamic 추가

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,11 @@
 import type { MetadataRoute } from "next";
 
+// 페이지가 정적이라는 것을 명시적으로 선언
+export const dynamic = "force-static";
+
+// ISR을 사용하여 일정 시간(초)마다 재생성하도록 설정
+// export const revalidate = 600;
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
