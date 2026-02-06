8a5bad4e93067da7e722fa9c7543ca1f314244b1
borland <46901223+gogogo2115@users.noreply.github.com>
2026-02-07 03:01:19 +0900

    Update page.tsx

--- app/color/contrast/page.tsx
+++ app/color/contrast/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "색상 대비 검사기",
+  description: "텍스트와 배경 색상의 대비 비율을 계산하세요.",
+  keywords: ["color contrast checker", "색상 대비 검사기"],
+};
+
 export default function ColorContrast() {
   return <></>;
 }
